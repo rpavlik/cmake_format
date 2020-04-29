85cfe688bee3e080686aee784844f02f292437ec
Rylie Pavlik <rylie.pavlik@collabora.com>
2020-04-29 13:52:26 -0500

    Changes to use as a lib

--- cmakelang/__init__.py
+++ cmakelang/__init__.py
@@ -1,6 +1,9 @@
 """
 Language tools for cmake
 """
+
 from __future__ import unicode_literals
+from .format.__main__ import process_file
+from .configuration import Configuration
 
 __version__ = "0.6.13"
