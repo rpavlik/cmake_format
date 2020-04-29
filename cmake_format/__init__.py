f12841bafa894a372b6941636a45e8fb6f50bcdc
Ryan Pavlik <ryan.pavlik@collabora.com>
2020-04-29 13:52:26 -0500

    Changes to use as a lib

--- cmake_format/__init__.py
+++ cmake_format/__init__.py
@@ -4,3 +4,6 @@ Parse cmake listfiles and format them nicely
 from __future__ import unicode_literals
 
 VERSION = '0.6.0'
+
+from .configuration import Configuration
+from .__main__ import process_file
